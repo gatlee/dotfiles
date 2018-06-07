1b66fcc0cfb8f3e8774d97c716f6b96ae27746ff
Your Name <gatleekaw@gmail.com>
2018-06-08 00:57:35 +1000

    Background new tabs

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,3 +2,4 @@ config.bind('<Ctrl-g>', 'leave-mode', mode='passthrough')
 config.bind('<Ctrl-g>', 'enter-mode passthrough') 
 config.unbind('<Ctrl-v>', mode='normal')
 config.unbind('<Ctrl-v>', mode='passthrough')
+c.tabs.background = True
